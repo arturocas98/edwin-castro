3a8bbfb6f8c4e682eb019e5c23dff0b64b2b67a2
Stackbit Code Editor <projects@stackbit.com>
2021-06-25 02:12:51 +0000

    siteConfig.js: changed

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -6,7 +6,7 @@ module.exports = {
   twitterUsername: '_franciscodf',
   githubUsername: 'santosfrancisco',
   authorAvatar: '/images/perfil.jpg',
-  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
+  authorDescription: `Desarrollar autodidacta apasionado <br />
     In 2012 I started working as a support technician and I approached the developers.
     In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
     I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
