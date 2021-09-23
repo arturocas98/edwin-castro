0b2a0dc7e3e7af2378f6837404149d1da156117c
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 02:32:32 +0000

    repositories.js: changed

--- src/components/repositories/repositories.js
+++ src/components/repositories/repositories.js
@@ -28,7 +28,7 @@ class Repositories extends React.Component {
     const { status } = this.state
     return (
       <div className={this.props.className}>
-        <h2>Ultimos Repositorios de Git</h2>
+        <h2>Ultimos Repositorios de Github</h2>
         {status === "loading" && <div className='repositories__loader'><Loader /></div>}
         {status === "ready" &&
           this.state.repos && (
