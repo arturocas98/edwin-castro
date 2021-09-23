a0d13c1ca5f14b9f97264c3acd5ab61544b4e058
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 02:29:43 +0000

    timeline.js: changed

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -13,8 +13,9 @@ const Timeline = ({ className }) => (
       >
         <div className="inner">
           <span className="timeline__date">
-            <span className="timeline__month">{job.begin.month} 2021</span>
+            <span className="timeline__month">{job.begin.month}</span>
             <span className="timeline__year">{job.begin.year}</span>
+            <span className="timeline__year" ></span>
           </span>
           <div className="timeline__card">
             <h2 className='timeline__card-title'>
