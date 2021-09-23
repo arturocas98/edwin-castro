c3cab92795254cae2e3b1fe83faaa6924b85d08c
Stackbit Code Editor <projects@stackbit.com>
2021-09-23 02:58:24 +0000

    siteConfig.js: changed

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -70,7 +70,6 @@ module.exports = {
     {
       image: "/images/gatsby-starter-cv.png",
       description: "Marketplace en Angular",
-      description2:"Angular es un framework para aplicaciones web desarrollado en TypeScript, de código abierto, mantenido por Google, que se utiliza para crear y mantener aplicaciones web de una sola página.",
       url: "https://www.gatsbyjs.org/starters/santosfrancisco/gatsby-starter-cv/"
     },
     {
